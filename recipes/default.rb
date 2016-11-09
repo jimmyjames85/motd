3d601c9a242b4dc50c5a950f816c3c7da7d9ccad
jimmyjames <james.tappe@sendgrid.com>
2016-11-09 09:07:08 -0800

    rm package install

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,8 +4,8 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-package 'cowsay'
-package 'figlet'
+# package 'cowsay'
+# package 'figlet'
 
 template '/etc/motd' do 
   source 'motd.erb'
